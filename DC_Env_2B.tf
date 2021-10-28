bf3545363d6c89275205be40f28755a53826dfba
waleedrady <82145296+waleedrady@users.noreply.github.com>
2021-10-28 16:33:07 -0400

    Create DC_Env_2B.tf

---
+++ DC_Env_2B.tf
@@ -0,0 +1,99 @@
+module "DC_Enviroment_2B" {
+
+  source  = "WEEMR/DataCenter_Enviroment_2B/aws"
+  version = "0.1.0"
+
+  # ------------------------------------------------------ AWS Account Settings ------------------------------------------ #
+
+  access_key = "YOUR ACCESS KEY"
+  secret_key = "YOUR SECRET KEY"
+
+  # ------------------------------------------------------ Variables ----------------------------------------------------- #
+
+  username = "jdoe"  #  Your 1st Initial, Last Name to tag resources i.e jdoe
+  keyname  = "YOUR AWS KEY NAME"   #  Your AWS Keypair name to create resources
+  Password = "Fortinet123!"        #  TO DO: Change the password from "fortinet123" to your own password
+
+  # ------------------------------------------------------  DNS ------------------------------------------------------------ #
+
+  Public_Hosted_Zone = "fortinetpslab.com"            # You must have a domain registerd with AWS Route53 or Managed by AWS with a Hosted Zone Created. i.e xyz.com
+  SubHosted_Zone     = "jdoe.fortinetpslab.com" # Creates a Public SubHosted zone  - Enter a sub-hosted name for the domain above. i.e lab.xyz.com
+
+  # ------------------------------------------------------ Reference Module ------------------------------------------------ #
+
+  #            Terraform Registery Module = https://registry.terraform.io/modules/WEEMR/DataCenter_Enviroment_2B/aws/latest
+
+  #                             Source Code = https://github.com/WEEMR/terraform-aws-DataCenter_Enviroment_2B
+
+}
+
+#  ----------------  Outputs  -----------------  # 
+
+output "AWS_US_East_Hub_1_VPC_ID" {
+  value = module.DC_Enviroment_2B.AWS_US_East_Hub_1_VPC_ID
+}
+
+output "AWS_US_East_Spoke_1_VPC_ID" {
+  value = module.DC_Enviroment_2B.AWS_US_East_Spoke_1_VPC_ID
+}
+
+
+
+# ---------------- ---------------- ---------------- Hub 1
+
+output "hub_1_WAN_1_IP" {
+  value = module.DC_Enviroment_2B.hub_1_WAN_1_EIP
+}
+
+output "hub_1_WAN_2_IP" {
+  value = module.DC_Enviroment_2B.hub_1_WAN_2_EIP
+}
+
+output "hub1_FGT_DNS_Name" {
+  value = module.DC_Enviroment_2B.hub1_FGT_DNS_Name
+}
+
+output "hub1_Linux_DNS_Name" {
+  value = module.DC_Enviroment_2B.hub1_Linux_DNS_Name
+}
+
+output "hub1_FMG_DNS_Name" {
+  value = module.DC_Enviroment_2B.hub1_FMG_DNS_Name
+}
+
+output "hub1_Windows_DNS_Name" {
+  value = module.DC_Enviroment_2B.hub1_Windows_DNS_Name
+}
+
+
+output "hub1_Windows_Password" {                                    # Your AWS KEY file below
+  value = rsadecrypt(module.DC_Enviroment_2B.hub1_Windows_Password, file("./AWS_Key.pem"))
+}
+
+# ---------------- ---------------- ---------------- spoke 1
+
+output "spoke_1_WAN_1_IP" {
+  value = module.DC_Enviroment_2B.spoke_1_WAN_1_EIP
+}
+
+output "spoke_1_WAN_2_IP" {
+  value = module.DC_Enviroment_2B.spoke_1_WAN_2_EIP
+}
+
+output "spoke1_FGT_DNS_Name" {
+  value = module.DC_Enviroment_2B.spoke1_FGT_DNS_Name
+}
+
+output "spoke1_Linux_DNS_Name" {
+  value = module.DC_Enviroment_2B.spoke1_Linux_DNS_Name
+}
+
+output "spoke1_Windows_DNS_Name" {
+  value = module.DC_Enviroment_2B.spoke1_Windows_DNS_Name
+}
+
+
+output "spoke1_Windows_Password" {                                    # Your AWS KEY file below
+  value = rsadecrypt(module.DC_Enviroment_2B.spoke1_Windows_Password, file("./AWS_KEY.pem"))
+}
+  
